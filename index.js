aa1d92de1390f3f47f3701a04963c4c6e360fbac
FCC <fabio.canesin@gmail.com>
2017-08-10 01:49:19 -0400

    push

---
+++ index.js
@@ -0,0 +1,148 @@
+var aes = require('browserify-aes')
+var assert = require('assert')
+var bs58check = require('bs58check')
+var createHash = require('create-hash')
+var scrypt = require('scryptsy')
+var xor = require('buffer-xor')
+
+var ecurve = require('ecurve')
+var curve = ecurve.getCurveByName('secp256r1')
+
+var BigInteger = require('bigi')
+
+// constants
+var SCRYPT_PARAMS = {
+  N: 16384, // specified by NEP2
+  r: 8,
+  p: 8
+}
+var NULL = new Buffer(0)
+
+function hash160 (buffer) {
+  return createHash('rmd160').update(
+    createHash('sha256').update(buffer).digest()
+  ).digest()
+}
+
+function hash256 (buffer) {
+  return createHash('sha256').update(
+    createHash('sha256').update(buffer).digest()
+  ).digest()
+}
+
+function getAddress (d) {
+  var Q = curve.G.multiply(d).getEncoded(false)
+  var hash = hash160(Q)
+  var payload = new Buffer(21)
+  payload.writeUInt8(0x00, 0)
+  hash.copy(payload, 1)
+
+  return bs58check.encode(payload)
+}
+
+function encryptRaw (buffer, passphrase, progressCallback, scryptParams) {
+  if (buffer.length !== 32) throw new Error('Invalid private key length')
+  scryptParams = scryptParams || SCRYPT_PARAMS
+
+  var d = BigInteger.fromBuffer(buffer)
+  var address = getAddress(d)
+  var secret = new Buffer(passphrase, 'utf8')
+  var salt = hash256(address).slice(0, 4)
+
+  var N = scryptParams.N
+  var r = scryptParams.r
+  var p = scryptParams.p
+
+  var scryptBuf = scrypt(secret, salt, N, r, p, 64, progressCallback)
+  var derivedHalf1 = scryptBuf.slice(0, 32)
+  var derivedHalf2 = scryptBuf.slice(32, 64)
+
+  var xorBuf = xor(buffer, derivedHalf1)
+  var cipher = aes.createCipheriv('aes-256-ecb', derivedHalf2, NULL)
+  cipher.setAutoPadding(false)
+  cipher.end(xorBuf)
+
+  var cipherText = cipher.read()
+
+  // 0x01 | 0x42 | flagByte | salt (4) | cipherText (32)
+  var result = new Buffer(7 + 32)
+  result.writeUInt8(0x01, 0)
+  result.writeUInt8(0x42, 1)
+  result.writeUInt8(0xe0, 2)
+  salt.copy(result, 3)
+  cipherText.copy(result, 7)
+
+  return result
+}
+
+function encrypt (buffer, passphrase, progressCallback, scryptParams) {
+  return bs58check.encode(encryptRaw(buffer, passphrase, progressCallback, scryptParams))
+}
+
+// some of the techniques borrowed from: https://github.com/pointbiz/bitaddress.org
+function decryptRaw (buffer, passphrase, progressCallback, scryptParams) {
+  // 39 bytes: 2 bytes prefix, 37 bytes payload
+  if (buffer.length !== 39) throw new Error('Invalid NEP2 data length')
+  if (buffer.readUInt8(0) !== 0x01) throw new Error('Invalid NEP2 prefix')
+  scryptParams = scryptParams || SCRYPT_PARAMS
+
+  var type = buffer.readUInt8(1)
+  if (type !== 0x42) throw new Error('Invalid NEP2 type')
+
+  passphrase = new Buffer(passphrase, 'utf8')
+
+  var flagByte = buffer.readUInt8(2)
+
+  var N = scryptParams.N
+  var r = scryptParams.r
+  var p = scryptParams.p
+
+  var salt = buffer.slice(3, 7)
+  var scryptBuf = scrypt(passphrase, salt, N, r, p, 64, progressCallback)
+  var derivedHalf1 = scryptBuf.slice(0, 32)
+  var derivedHalf2 = scryptBuf.slice(32, 64)
+
+  var privKeyBuf = buffer.slice(7, 7 + 32)
+  var decipher = aes.createDecipheriv('aes-256-ecb', derivedHalf2, NULL)
+  decipher.setAutoPadding(false)
+  decipher.end(privKeyBuf)
+
+  var plainText = decipher.read()
+  var privateKey = xor(plainText, derivedHalf1)
+
+  // verify salt matches address
+  var d = BigInteger.fromBuffer(privateKey)
+  var address = getAddress(d)
+  var checksum = hash256(address).slice(0, 4)
+  assert.deepEqual(salt, checksum)
+
+  return privateKey
+}
+
+function decrypt (string, passphrase, progressCallback, scryptParams) {
+  return decryptRaw(bs58check.decode(string), passphrase, progressCallback, scryptParams)
+}
+
+function verify (string) {
+  var decoded = bs58check.decodeUnsafe(string)
+  if (!decoded) return false
+
+  if (decoded.length !== 39) return false
+  if (decoded.readUInt8(0) !== 0x01) return false
+
+  var type = decoded.readUInt8(1)
+  var flag = decoded.readUInt8(2)
+
+  // encrypted WIF
+  if (type !== 0x42) return false
+  if (flag !== 0xe0) return false
+
+  return true
+}
+
+module.exports = {
+  getAddress: getAddress,
+  decrypt: decrypt,
+  encrypt: encrypt,
+  verify: verify
+}
---
+++ test/index.js
@@ -0,0 +1,63 @@
+/* global describe, it */
+
+var assert = require('assert')
+var bip38 = require('../')
+var bs58check = require('bs58check')
+var fixtures = require('./fixtures')
+var wif = require('wif')
+
+describe('bip38', function () {
+  this.timeout(200000)
+
+  describe('decrypt', function () {
+    fixtures.valid.forEach(function (f) {
+      it('should decrypt ' + f.description, function () {
+        var result = bip38.decrypt(f.bip38, f.passphrase)
+
+        assert.equal(wif.encode(0x80, result.privateKey, result.compressed), f.wif)
+      })
+    })
+
+    fixtures.invalid.decrypt.forEach(function (f) {
+      it('should throw ' + f.description, function () {
+        assert.throws(function () {
+          bip38.decrypt(f.bip38, f.passphrase)
+        }, new RegExp(f.description, 'i'))
+      })
+    })
+
+    fixtures.invalid.verify.forEach(function (f) {
+      it('should throw because ' + f.description, function () {
+        assert.throws(function () {
+          bip38.decrypt(f.base58, 'foobar')
+        }, new RegExp(f.exception))
+      })
+    })
+  })
+
+  describe('encrypt', function () {
+    fixtures.valid.forEach(function (f) {
+      if (f.decryptOnly) return
+
+      it('should encrypt ' + f.description, function () {
+        var buffer = bs58check.decode(f.wif)
+
+        assert.equal(bip38.encrypt(buffer.slice(1, 33), !!buffer[33], f.passphrase), f.bip38)
+      })
+    })
+  })
+
+  describe('verify', function () {
+    fixtures.valid.forEach(function (f) {
+      it('should return true for ' + f.bip38, function () {
+        assert(bip38.verify(f.bip38))
+      })
+    })
+
+    fixtures.invalid.verify.forEach(function (f) {
+      it('should return false for ' + f.description, function () {
+        assert(!bip38.verify(f.base58))
+      })
+    })
+  })
+})
